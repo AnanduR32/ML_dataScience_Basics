46d11bf1428e305533e486322054838fe1b64638
Anandu R <ananduraj32@gmail.com>
2019-11-01 21:30:22 +0530

    Octave #3

---
+++ Octave_ML/Basics/Matrix_mul.m
@@ -0,0 +1,19 @@
+% Initialize matrix A 
+A = [1, 2, 3; 4, 5, 6;7, 8, 9] 
+
+% Initialize vector v 
+v = [1; 2; 3] 
+
+% Multiply A * v
+Av = A * v
+
+% Multiplying a 3x3 matrix with a 3-d vector gives a 3-d vector 
+
+% Initialize a 3 by 2 matrix 
+A = [1, 2; 3, 4;5, 6]
+
+% Initialize a 2 by 1 matrix 
+B = [1; 2] 
+
+% We expect a resulting matrix of 3x1 matrix 
+mult_AB = A*B
