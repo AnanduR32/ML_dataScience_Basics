bb5dca5ef41e330592166dec7052d40956231ed9
Anandu R <ananduraj32@gmail.com>
2019-11-01 21:20:49 +0530

    Octave #2

---
+++ Octave_ML/Basics/Scalar_arithematic.m
@@ -0,0 +1,21 @@
+% Initialize matrix A and B 
+A = [1, 2, 3; 4, 6, 7]
+B = [8, 9, 10; 11, 12, 13]
+
+% Initialize constant s 
+s = 10
+
+% See how element-wise addition works
+add_AB = A + B 
+
+% See how element-wise subtraction works
+sub_AB = A - B
+
+% See how scalar multiplication works
+mult_As = A * s
+
+% Divide A by s
+div_As = A / s
+
+% What happens if we have a Matrix + scalar?
+add_As = A + s
