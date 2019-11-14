af94c93cdedf5b49b54080f08f3eaafaadd89827
Anandu R <ananduraj32@gmail.com>
2019-11-14 09:23:33 +0530

    update

---
+++ Octave_ML/Algorithms/GradientDescentLog.m
@@ -0,0 +1,20 @@
+function [theta,J] = GradientDescentLog(X,y,alpha,theta)
+  m=size(X,1);
+  z=zeros(m,1);
+  for i=1:1000,
+    z=X*theta;
+    pred=1/(1+exp(-z));
+    if(pred>=0.5)
+      pred=1;
+    else
+      pred=0;
+    endif
+    Error=(pred-y);
+    Delta=Error'*X;
+    Delta=Delta*(1/m);
+    theta=theta-alpha*Delta';
+    J=computeCostLog(X,y,theta);
+  end;
+endfunction
+
+
