e12cbd35d7b979da8505f6c2ed1fe8923ef09a05
Anandu R <ananduraj32@gmail.com>
2019-11-14 19:24:57 +0530

    update

--- Octave_ML/Algorithms/titanic-data/GradientDescent.m
+++ Octave_ML/Algorithms/titanic-data/GradientDescent.m
@@ -1,10 +1,23 @@
-function [theta,J] = GradientDescent(X,y,alpha,theta)
-  m=size(X,1);
+function [theta,J] = GradientDescent()
+  data=csvread('train.csv');
+  X=[data(:,3),data(:,7)];
+  m=length(X);
+  X=[ones(m,1),X];
+  alpha=0.001;
+  theta=zeros(3,1);
+  i=1;
+  y=data(:,2);
+  while(i<=m)
+    if(isna(X(i,3)))
+      X(i,3)=avg_age;
+    endif
+    i=i+1;
+  end
   z=zeros(m,1);
-  for i=1:1000,
+  for i=1:100000,
     z=X*theta;
     pred=1/(1+exp(-z));
-    if(pred>=0.5)
+    if(pred>=0)
       pred=1;
     else
       pred=0;
