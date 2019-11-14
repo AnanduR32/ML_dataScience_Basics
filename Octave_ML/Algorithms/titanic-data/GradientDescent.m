37cb72f77b6f285090f19b306caac20d25bd279e
Anandu R <ananduraj32@gmail.com>
2019-11-14 22:48:35 +0530

    LOGISTICRegression

--- Octave_ML/Algorithms/titanic-data/GradientDescent.m
+++ Octave_ML/Algorithms/titanic-data/GradientDescent.m
@@ -1,30 +1,16 @@
 function [theta,J] = GradientDescent()
   data=csvread('train.csv');
-  X=[data(:,3),data(:,7)];
-  m=length(X);
+  m=length(data);
+  X=[data(1:m,3),data(1:m,7)];
   X=[ones(m,1),X];
-  alpha=0.001;
-  theta=zeros(3,1);
-  i=1;
+  alpha=0.075;
+  theta=ones(3,1);
   y=data(:,2);
-  while(i<=m)
-    if(isna(X(i,3)))
-      X(i,3)=avg_age;
-    endif
-    i=i+1;
-  end
-  z=zeros(m,1);
-  for i=1:100000,
-    z=X*theta;
-    pred=1/(1+exp(-z));
-    if(pred>=0)
-      pred=1;
-    else
-      pred=0;
-    endif
-    Error=(pred-y);
-    Delta=Error'*X;
-    Delta=Delta*(1/m);
+  X(X(:,3)==0,3)=mean(X(:,3));
+  z=ones(m,1);
+  for i=1:10000000,
+    pred=(1./(1+exp(-(X*theta))))>0.5;
+    Delta=((pred-y)'*X)./m;
     theta=theta-alpha*Delta';
     %J=computeCostLog(X,y,theta);
   end;
